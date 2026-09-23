582f7b57054eef7ceddb51edd61a389e08b7b122
karelys Jimenez <sunwork50ppd@gmail.com>
2026-09-23 13:36:53 -0500

    nota minima y maxima

--- Program.cs
+++ Program.cs
@@ -25,4 +25,19 @@ ImprimirNotas(notas);
 double promedio = PromedioNotas (notas); 
 Console.WriteLine($"Promedio de las notas : {promedio:F2}"); 
 
-
+//imprimir minino y maximo 
+int min = notas[0]; 
+int max= notas[0]; 
+for(int i = 1 ; i < notas.Length; i++ )
+{
+    if(notas[i]< min)
+    {
+        min= notas[i]; 
+    }
+    if(notas[i] > max)
+    {
+        max= notas[i]; 
+    }
+}
+Console.WriteLine($"minimo de notas: {min}"); 
+Console.WriteLine($"maximo de notas: {max}"); 

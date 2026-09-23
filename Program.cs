27d8d00d746e3aa0408eefef49a041f2a0e545e6
karelys Jimenez <sunwork50ppd@gmail.com>
2026-09-23 12:37:03 -0500

    impresion

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿static void ImprimirNotas(int[] notasx)
+{
+
+for(int i = 0; i < notasx.Length; i++)
+{
+    Console.WriteLine($"nota {i + 1 }; {notasx[i]}"); 
+}
+}
+
+int[] notas = {15,18,12,17,20}; 
+
+ImprimirNotas(notas); 
+
+int[] notas2 = {2,4,9};
+
+ImprimirNotas(notas2);  
